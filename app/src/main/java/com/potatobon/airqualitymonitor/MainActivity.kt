dcad9f92d31c8ad23f668465f95e5ad409f79a1d
Qian Jin <qian.jin.dev@gmail.com>
2018-01-31 00:35:33 +0100

    humidity reading seems to be off
    need further check on driver usage

--- app/src/main/java/com/potatobon/airqualitymonitor/MainActivity.kt
+++ app/src/main/java/com/potatobon/airqualitymonitor/MainActivity.kt
@@ -9,17 +9,33 @@ import java.io.IOException
 
 class MainActivity : Activity() {
 
+    private lateinit var bme680: Bme680
+    
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        val bme680: Bme680
 
         try {
             bme680 = Bme680(BoardDefaults.getI2cBus())
             bme680.temperatureOversample = Bme680.OVERSAMPLING_1X
             val temperature = bme680.readTemperature()
-            temperatureTextView.text = "Current Temperature: $temperature"
+
+            bme680.humidityOversample = Bme680.OVERSAMPLING_1X
+            val humidity = bme680.readHumidity()
+
+            bme680.pressureOversample = Bme680.OVERSAMPLING_1X
+            val pressure = bme680.readPressure()
+
+            readings.text = "Temperature: $temperature\nHumidity: $humidity\nPressure: $pressure"
+        } catch (e: IOException) {
+            Log.e("AirQualityMonitor", e.message)
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        try {
+            bme680.close()
         } catch (e: IOException) {
             Log.e("AirQualityMonitor", e.message)
         }
